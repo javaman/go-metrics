bde3bb0ba69bfc587dbad286f3dfedcf530bc728
Maksim <b0wser@mail.ru>
2023-05-01 19:34:19 +0300

    Tests fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,62 +11,72 @@ type MemStorage struct {
 	counter int64
 }
 
-func (h *MemStorage) ServeHTTP(res http.ResponseWriter, r *http.Request) {
+func validate(r *http.Request) (string, bool, float64, int64, int) {
 	if r.Method != http.MethodPost {
-		res.WriteHeader(http.StatusMethodNotAllowed)
+		return "", false, 0, 0, http.StatusMethodNotAllowed
+	} else {
+		parts := strings.Split(r.URL.Path, "/")
+		metricType := ""
+		metricName := ""
+		metricValue := ""
+		if len(parts) >= 3 {
+			metricType = strings.TrimSpace(parts[2])
+		}
+		if len(parts) >= 4 {
+			metricName = strings.TrimSpace(parts[3])
+		}
+		if len(parts) >= 5 {
+			metricValue = strings.TrimSpace(parts[4])
+		}
+		return validateParams(metricType, metricName, metricValue)
+	}
+}
+
+func validateParams(metricType string, metricName string, metricValue string) (name string, isGauge bool, gaugeValue float64, counter int64, status int) {
+	name = metricName
+	isGauge = false
+	gaugeValue = 0
+	counter = 0
+	status = http.StatusOK
+
+	switch metricType {
+	case "gauge":
+		isGauge = true
+	case "counter":
+		isGauge = false
+	default:
+		status = http.StatusBadRequest
 		return
 	}
-	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) <= 2 || (parts[2] != "gauge" && parts[2] != "counter") {
-		res.WriteHeader(http.StatusBadRequest)
+
+	if metricName == "" {
+		status = http.StatusNotFound
 		return
 	}
-	if len(parts) == 4 && parts[2] == "gauge" {
-		if _, err := strconv.ParseFloat(parts[3], 64); err == nil {
-			res.WriteHeader(http.StatusNotFound)
+
+	if isGauge {
+		if value, err := strconv.ParseFloat(metricValue, 64); err == nil {
+			gaugeValue = value
 		} else {
-			if strings.TrimSpace(parts[3]) == "" {
-				res.WriteHeader(http.StatusNotFound)
-			} else {
-				res.WriteHeader(http.StatusBadRequest)
-			}
+			status = http.StatusBadRequest
+			return
 		}
-		return
-	}
-	if len(parts) == 4 && parts[2] == "counter" {
-		if _, err := strconv.ParseInt(parts[3], 10, 64); err == nil {
-			res.WriteHeader(http.StatusNotFound)
+	} else {
+		if value, err := strconv.ParseInt(metricValue, 10, 64); err == nil {
+			counter = value
 		} else {
-			if strings.TrimSpace(parts[3]) == "" {
-				res.WriteHeader(http.StatusNotFound)
-			} else {
-				res.WriteHeader(http.StatusBadRequest)
-			}
-		}
-		return
-	}
-
-	if parts[2] == "gauge" {
-		if _, err := strconv.ParseFloat(parts[3], 64); err != nil {
-			if strings.TrimSpace(parts[3]) == "" {
-				res.WriteHeader(http.StatusNotFound)
-			} else {
-				res.WriteHeader(http.StatusBadRequest)
-			}
+			status = http.StatusBadRequest
+			return
 		}
 	}
 
-	if parts[2] == "counter" {
-		if _, err := strconv.ParseInt(parts[3], 10, 64); err != nil {
-			if strings.TrimSpace(parts[3]) == "" {
-				res.WriteHeader(http.StatusNotFound)
-			} else {
-				res.WriteHeader(http.StatusBadRequest)
-			}
-		}
-	}
+	return
+}
 
+func (h *MemStorage) ServeHTTP(res http.ResponseWriter, r *http.Request) {
+	_, _, _, _, status := validate(r)
 	res.Header().Set("Content-Type", "text/plain")
+	res.WriteHeader(status)
 }
 
 func main() {
