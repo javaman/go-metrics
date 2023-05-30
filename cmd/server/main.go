5fe0b030e4a1e2194338965f8d5e4a3f4219b265
Maksim <b0wser@mail.ru>
2023-05-30 11:45:24 +0300

    refactor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/javaman/go-metrics/internal/config"
 	"github.com/javaman/go-metrics/internal/handlers"
 	"github.com/javaman/go-metrics/internal/repository"
 	"github.com/javaman/go-metrics/internal/services"
-	"go.uber.org/zap"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-
 	cfg := config.ConfigureServer()
 
 	var storage repository.Storage
@@ -29,57 +21,14 @@ func main() {
 	var service services.MetricsService
 
 	if cfg.StoreInterval > 0 {
-		fmt.Println("Here")
 		service = services.NewMetricsService(storage)
 		services.FlushStorageInBackground(storage, cfg.FileStoragePath, cfg.StoreInterval)
 	} else {
-		fmt.Println("There")
 		storage = repository.MakeStorageFlushedOnEachCall(storage, cfg.FileStoragePath)
 		service = services.NewMetricsService(storage)
 	}
 
-	e := echo.New()
-
-	e.GET("/", handlers.ListAll(service))
-
-	e.GET("/value/counter/:measureName", handlers.ValueCounter(service))
-	e.GET("/value/gauge/:measureName", handlers.ValueGauge(service))
-	e.POST("/value/", handlers.Value(service))
-
-	e.GET("/update/*", func(c echo.Context) error { return c.NoContent(http.StatusMethodNotAllowed) })
-	e.POST("/update/:measureType/*", handlers.BadRequest)
-
-	e.POST("/update/counter/:measureName/:measureValue", handlers.UpdateCounter(service))
-	e.POST("/update/counter/", handlers.NotFound)
-	e.POST("/update/gauge/:measureName/:measureValue", handlers.UpdateGauge(service))
-	e.POST("/update/gauge/", handlers.NotFound)
-	e.POST("/update/", handlers.Update(service))
-
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
-	sugar := logger.Sugar()
-
-	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-		LogURI:          true,
-		LogMethod:       true,
-		LogLatency:      true,
-		LogStatus:       true,
-		LogResponseSize: true,
-		LogValuesFunc: func(e echo.Context, v middleware.RequestLoggerValues) error {
-			sugar.Infow("request",
-				zap.String("URI", v.URI),
-				zap.String("method", v.Method),
-				zap.Int64("latency", v.Latency.Nanoseconds()),
-			)
-			sugar.Infow("response",
-				zap.Int("status", v.Status),
-				zap.Int64("size", v.ResponseSize),
-			)
-			return nil
-		},
-	}))
-	e.Use(handlers.Compress)
-	e.Use(handlers.Decompress)
+	e := handlers.New(service)
 
 	e.Logger.Fatal(e.Start(cfg.Address))
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,6 +12,8 @@ import (
 	"github.com/javaman/go-metrics/internal/model"
 	"github.com/javaman/go-metrics/internal/services"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"go.uber.org/zap"
 )
 
 const (
@@ -242,4 +244,50 @@ func Value(s services.MetricsService) func(echo.Context) error {
 			return c.JSON(http.StatusOK, res)
 		}
 	}
+}
+
+func New(service services.MetricsService) *echo.Echo {
+	e := echo.New()
+
+	e.GET("/", ListAll(service))
+
+	e.GET("/value/counter/:measureName", ValueCounter(service))
+	e.GET("/value/gauge/:measureName", ValueGauge(service))
+	e.POST("/value/", Value(service))
+
+	e.GET("/update/*", func(c echo.Context) error { return c.NoContent(http.StatusMethodNotAllowed) })
+	e.POST("/update/:measureType/*", BadRequest)
+
+	e.POST("/update/counter/:measureName/:measureValue", UpdateCounter(service))
+	e.POST("/update/counter/", NotFound)
+	e.POST("/update/gauge/:measureName/:measureValue", UpdateGauge(service))
+	e.POST("/update/gauge/", NotFound)
+	e.POST("/update/", Update(service))
+
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:          true,
+		LogMethod:       true,
+		LogLatency:      true,
+		LogStatus:       true,
+		LogResponseSize: true,
+		LogValuesFunc: func(e echo.Context, v middleware.RequestLoggerValues) error {
+			sugar.Infow("request",
+				zap.String("URI", v.URI),
+				zap.String("method", v.Method),
+				zap.Int64("latency", v.Latency.Nanoseconds()),
+			)
+			sugar.Infow("response",
+				zap.Int("status", v.Status),
+				zap.Int64("size", v.ResponseSize),
+			)
+			return nil
+		},
+	}))
+	e.Use(Compress)
+	e.Use(Decompress)
+	return e
 }
