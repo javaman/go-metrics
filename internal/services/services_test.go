a14779f765f0723c6438500abb898b40710d0d6c
Maksim <b0wser@mail.ru>
2023-06-12 10:25:49 +0300

    iter13

--- internal/services/services_test.go
+++ internal/services/services_test.go
@@ -42,15 +42,15 @@ func (m *mockStorage) WriteToFile(fname string) {
 	m.Called(fname)
 }
 
-func (m *mockStorage) Lock() repository.LockedStorage {
+func (m *mockStorage) Lock() (repository.LockedStorage, error) {
 	m.Called()
-	return nil
+	return nil, nil
 }
 
 func TestSaveGauge(t *testing.T) {
 	theMock := &mockStorage{}
 	theMock.On("SaveGauge", "one", 3.14)
-	ms := NewMetricsService(theMock, func() error { return nil })
+	ms := NewMetricsService(theMock)
 	ms.SaveGauge("one", 3.14)
 	theMock.AssertCalled(t, "SaveGauge", "one", 3.14)
 	theMock.AssertExpectations(t)
@@ -60,7 +60,7 @@ func TestSaveGauge(t *testing.T) {
 func TestGetGauge(t *testing.T) {
 	theMock := &mockStorage{}
 	theMock.On("GetGauge", "one").Return(3.14, true)
-	ms := NewMetricsService(theMock, func() error { return nil })
+	ms := NewMetricsService(theMock)
 	if v, ok := ms.GetGauge("one"); ok {
 		assert.Equal(t, v, 3.14, "That should not happen")
 	} else {
@@ -75,7 +75,7 @@ func TestAllGauges(t *testing.T) {
 	theMock := &mockStorage{}
 	f := func(k string, v float64) {}
 	theMock.On("AllGauges", mock.Anything)
-	ms := NewMetricsService(theMock, func() error { return nil })
+	ms := NewMetricsService(theMock)
 	ms.AllGauges(f)
 	theMock.AssertCalled(t, "AllGauges", mock.Anything)
 	theMock.AssertExpectations(t)
@@ -86,7 +86,7 @@ func TestSaveCounter(t *testing.T) {
 	theMock := &mockStorage{}
 	theMock.On("GetCounter", "one").Return(int64(0), false)
 	theMock.On("SaveCounter", "one", int64(1))
-	ms := NewMetricsService(theMock, func() error { return nil })
+	ms := NewMetricsService(theMock)
 	ms.SaveCounter("one", 1)
 	theMock.AssertCalled(t, "GetCounter", "one")
 	theMock.AssertCalled(t, "SaveCounter", "one", int64(1))
@@ -98,7 +98,7 @@ func TestSaveCounterUpdate(t *testing.T) {
 	theMock := &mockStorage{}
 	theMock.On("GetCounter", "one").Return(int64(3), true)
 	theMock.On("SaveCounter", "one", int64(4))
-	ms := NewMetricsService(theMock, func() error { return nil })
+	ms := NewMetricsService(theMock)
 	ms.SaveCounter("one", 1)
 	theMock.AssertCalled(t, "GetCounter", "one")
 	theMock.AssertCalled(t, "SaveCounter", "one", int64(4))
@@ -109,7 +109,7 @@ func TestSaveCounterUpdate(t *testing.T) {
 func TestGetCounter(t *testing.T) {
 	theMock := &mockStorage{}
 	theMock.On("GetCounter", "one").Return(int64(42), true)
-	ms := NewMetricsService(theMock, func() error { return nil })
+	ms := NewMetricsService(theMock)
 	if v, ok := ms.GetCounter("one"); ok {
 		assert.Equal(t, v, int64(42), "That should not happen")
 	} else {
@@ -124,7 +124,7 @@ func TestAllCounters(t *testing.T) {
 	theMock := &mockStorage{}
 	f := func(k string, v int64) {}
 	theMock.On("AllCounters", mock.Anything)
-	ms := NewMetricsService(theMock, func() error { return nil })
+	ms := NewMetricsService(theMock)
 	ms.AllCounters(f)
 	theMock.AssertCalled(t, "AllCounters", mock.Anything)
 	theMock.AssertExpectations(t)
